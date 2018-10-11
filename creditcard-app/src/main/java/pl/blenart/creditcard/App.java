96d31ab9c8a50bdfd8a71c26cf5caa244ebf11c1
Bartosz Lenart <b.lenart@yahoo.pl>
2018-10-11 10:40:10 +0000

    add assign limit to the card

---
+++ creditcard-app/src/main/java/pl/blenart/creditcard/App.java
@@ -0,0 +1,13 @@
+package pl.blenart.creditcard;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
