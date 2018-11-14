df75f4d423ea726436c4413745c4506eb4c4dad4
Marcin Skrzekut <marcin.skrzekut@gmail.com>
2018-11-14 22:16:20 +0100

    added withdraw and repay features, also some fixes

--- creditcard-app/src/main/java/pl/blenart/creditcard/CreditCard.java
+++ creditcard-app/src/main/java/pl/blenart/creditcard/CreditCard.java
@@ -1,11 +1,36 @@
 package pl.blenart.creditcard;
 
 class CreditCard {
-    public void assignLimit(double money) {
-        
+    private boolean blocked = false;
+    
+    private double balance = 0;
+    private double cardlimit = 0;
+    public void assignLimit(double limit) {
+        balance = limit;
+        cardlimit = limit;
+    }
+    
+    public boolean isBlocked() {
+        return this.blocked;
+    }
+    
+    public void block(){
+        this.blocked = true;
     }
     
     public double getLimit() {
         return 2000;
+    }
+    
+    public void withdraw(double money){
+        balance = balance - money;
+    }
+    
+    public void repay(double money) {
+        balance = balance + money;
+    }
+    
+    public double getBalance() {
+        return balance;
     }
 }
--- creditcard-app/src/test/java/pl/blenart/creditcard/CreditCardTest.java
+++ creditcard-app/src/test/java/pl/blenart/creditcard/CreditCardTest.java
@@ -5,15 +5,45 @@ import org.junit.Assert;
 import org.junit.Test;
 
 
-public class CreditCardTest{
+public class CreditCardTest {
     
     @Test
-    public void allowAssignLimitToCard(){
-        
+    public void assignCreditLimitToCard() {
         CreditCard card = new CreditCard();
         
-        card.assignLimit(2000.0);
-        Assert.assertTrue(card.getLimit() == 2000.0);
+        card.assignLimit(2000);
+        
+        Assert.assertTrue(card.getLimit() == 2000);
     }
     
+    @Test
+    public void cantWithdrawWhenCantAfford() {
+        
+    }
+    
+    @Test
+    public void canBlockCredsitCard() {
+        CreditCard card = new CreditCard();
+        card.block();
+        Assert.assertTrue(card.isBlocked());
+    }
+    @Test
+    public void withdrawDecreaseAvailableFunds() {
+        CreditCard card = new CreditCard();
+        
+        card.assignLimit(2000);
+        card.withdraw(1000);
+        
+        Assert.assertTrue(card.getBalance() == 1000);
+    }
+    
+    @Test
+    public void repayCredit() {
+        CreditCard card = new CreditCard();
+        card.assignLimit(1000);
+        card.withdraw(500);
+        card.withdraw(200);
+        card.repay(400);
+        Assert.assertTrue(card.getBalance() == 700);
+    }
 }
